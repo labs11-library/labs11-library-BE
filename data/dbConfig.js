1ce014d318efd4ac18d50f4eb0c5dc506c1dca8a
Henry Neal <henry.neal0@gmail.com>
2019-03-20 17:27:23 -0400

    deleted procfile and updated dbConfig

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const knex = require("knex");
 const config = require("../knexfile");
 
-const dbEnv = process.env.DB_ENV || "development";
+// const dbEnv = process.env.DB_ENV || "development";
 
-module.exports = knex(config[dbEnv]);
+// module.exports = knex(config[dbEnv]);
+const environment = process.env.ENVIRONMENT || "development";
+
+module.exports = knex(knexConfig[environment]);
