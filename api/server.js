8747821f689f6b6043e7345339658b507397a453
Scott Vojik <scott.vojik@gmail.com>
2019-03-21 11:00:37 -0700

    pg testing

--- api/server.js
+++ api/server.js
@@ -6,16 +6,16 @@ const db = require("../data/dbConfig");
 config(server);
 
 server.get("/", (req, res) => {
-	res.status(200).json({ api: "running" });
+  res.status(200).json({ api: "running" });
 });
 
 server.get("/users", async (req, res) => {
-	try {
-		const users = await db("users").orderBy("userId");
-		res.status(200).json(users);
-	} catch (error) {
-		res.status(500).json({ message: "no users displayed!" });
-	}
+  try {
+    const users = await db("users").orderBy("userId");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "no users displayed!" });
+  }
 });
 
 module.exports = server;
