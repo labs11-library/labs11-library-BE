ed8ece711e5019201692f1783935f36f146be3e3
Scott Vojik <scott.vojik@gmail.com>
2019-03-22 09:06:46 -0700

    fixed google strategy for Oauth, completed google Oauth

--- api/auth/passportConfig.js
+++ api/auth/passportConfig.js
@@ -1,28 +1,43 @@
 require("dotenv").config();
-const db = require('./query');
+const db = require("./query");
 
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
-passport.use(new GoogleStrategy({
-  // Google+ API Keys
-  callbackURL: process.env.GOOGLE_CALLBACK_URL,
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }, (accessToken, refreshToken, profile, done) => {
-    // callback
-    db.findUserByGoogleId(profile.id).then((id) => {
-      // console.log('profile', profile)
-      if (id) {
-        return done(null, profile);
-      } else {
-        db.createUserWithGoogleId(profile.id).then((id) => {
+passport.use(
+  new GoogleStrategy(
+    {
+      // Google+ API Keys
+      callbackURL: process.env.GOOGLE_CALLBACK_URL || "/auth/google/redirect",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    },
+    (accessToken, refreshToken, profile, done) => {
+      // callback
+      db.findUserByGoogleId(profile.id).then(id => {
+        console.log("profile", profile);
+        if (id) {
           return done(null, profile);
-        });
-      }
-    })
-  })
-)
+        } else {
+          db.getUsers()
+            .insert(
+              {
+                googleId: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                picture: profile.photos[0].value
+              },
+              "*"
+            )
+            .then(users => {
+              return done(null, users[0]);
+            });
+        }
+      });
+    }
+  )
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
--- api/auth/query.js
+++ api/auth/query.js
@@ -1,13 +1,14 @@
-
 const knex = require("../../data/dbConfig");
 
 module.exports = {
-
-  findUserByGoogleId: (profileId) => {
-    return knex('users').select().where({ googleId: profileId }).first();
+  findUserByGoogleId: profileId => {
+    return knex("users")
+      .select()
+      .where({ googleId: profileId })
+      .first();
   },
 
-  createUserWithGoogleId: (profileId) => {
-    return knex('users').insert({ googleId: profileId });
+  getUsers: () => {
+    return knex("users");
   }
 };
