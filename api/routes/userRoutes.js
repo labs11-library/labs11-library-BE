921b6317a219835f22eaef62c46f815ee627bdee
Scott Vojik <scott.vojik@gmail.com>
2019-03-25 18:23:19 -0700

    created users POST and PUT routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -18,6 +18,23 @@ router.get("/", async (req, res) => {
   }
 });
 
+//CREATE user (POST)
+
+router.post("/", async (req, res) => {
+  try {
+    const user = await db("users").insert(req.body);
+    if (user) {
+      return res.status(200).json({ message: "User created successfully." });
+    } else {
+      return res
+        .status(404)
+        .json({ error: "The user could not be created at this time." });
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
 //GET user by id
 
 router.get("/:id", async (req, res) => {
@@ -36,6 +53,30 @@ router.get("/:id", async (req, res) => {
     res
       .status(500)
       .json({ message: "Could not retrieve the user at this time." });
+  }
+});
+
+//EDIT USER
+
+router.put("/:id", async (req, res) => {
+  try {
+    const edited = await db("users")
+      .where({ userId: req.params.id })
+      .update(req.body);
+    const editedUser = await db("users")
+      .where({ userId: req.params.id })
+      .first();
+    if (edited) {
+      return res
+        .status(200)
+        .json({ message: "User successfully edited!", editedUser });
+    } else {
+      return res
+        .status(404)
+        .json({ error: "The user with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
