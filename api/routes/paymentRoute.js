888689663562d49cf242d677250cdb00b5a20296
Henry Neal <henry.neal0@gmail.com>
2019-03-29 13:49:48 -0400

    changed body-parser to .text and brought in dotenv config.  refactored post to /charge

--- api/routes/paymentRoute.js
+++ api/routes/paymentRoute.js
@@ -1,29 +1,45 @@
+require("dotenv").config();
 const express = require("express");
 const router = express.Router();
 const stripe = require("stripe")(process.env.SECRET_KEY);
 
-router.set("view engine", "ejs");
-router.use(require("body-parser").urlencoded({ extended: false }));
+// router.set("view engine", "ejs");
+router.use(require("body-parser").text());
 
 router.get("/", (req, res) => res.render("index", process.env.PUBLISHABLE_KEY));
 
-router.post("/charge", (req, res) => {
-	let amount = 500;
+router.post("/charge", async (req, res) => {
+	try {
+		let { status } = await stripe.charges.create({
+			amount: 2000,
+			currency: "usd",
+			description: "An example charge",
+			source: req.body
+		});
 
-	stripe.customers
-		.create({
-			email: req.body.stripeEmail,
-			source: req.body.stripeToken
-		})
-		.then(customer =>
-			stripe.charges.create({
-				amount,
-				description: "Sample Charge",
-				currency: "usd",
-				customer: customer.id
-			})
-		)
-		.then(charge => res.render("charge"));
+		res.json({ status });
+	} catch (err) {
+		res.status(500).end();
+	}
 });
+
+// router.post("/charge", (req, res) => {
+// 	let amount = 500;
+
+// 	stripe.customers
+// 		.create({
+// 			email: req.body.stripeEmail,
+// 			source: req.body.stripeToken
+// 		})
+// 		.then(customer =>
+// 			stripe.charges.create({
+// 				amount,
+// 				description: "Sample Charge",
+// 				currency: "usd",
+// 				customer: customer.id
+// 			})
+// 		)
+// 		.then(charge => res.render("charge"));
+// });
 
 module.exports = router;
