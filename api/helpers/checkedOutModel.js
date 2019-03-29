5717a343d98c8ebbd400646e6c2418d67b651d4f
Scott Vojik <scott.vojik@gmail.com>
2019-03-29 09:00:48 -0700

    Fixed get checkedOut item by checkedOutId model to return data we want

--- api/helpers/checkedOutModel.js
+++ api/helpers/checkedOutModel.js
@@ -4,7 +4,8 @@ const knexConfig = require("../../knexfile");
 const db = knex(knexConfig.development);
 
 module.exports = {
-  getCheckedOut
+  getCheckedOut,
+  getCheckedOutById
 };
 
 function getCheckedOut(userId) {
@@ -33,6 +34,30 @@ function getCheckedOut(userId) {
     )
     .where("borrowers.userId", userId)
     .orWhere("lenders.userId", userId);
+
+  return items;
+}
+
+function getCheckedOutById(checkedOutId) {
+  const items = db("checkedOut")
+    .join("users as borrowers", "checkedOut.borrowerId", "borrowers.userId")
+    .join("users as lenders", "checkedOut.lenderId", "lenders.userId")
+    .join("books", "checkedOut.bookId", "books.bookId")
+    .select(
+      "borrowers.firstName as borrower", //firstName from Users table, person borrowing,
+      "checkedOut.borrowerId as borrowerId", //ID from users Table, person borrowing ID
+      "books.bookId", //ID from inventory table, ID of the book being borrowed
+      "books.title",
+      "books.authors",
+      "books.description",
+      "checkedOut.checkedOutId",
+      "checkedOut.checkoutDate",
+      "checkedOut.dueDate",
+      "checkedOut.lenderId as lenderId",
+      "lenders.firstName as lender"
+    )
+    .where("checkedOut.checkedOutId", checkedOutId)
+    .first();
 
   return items;
 }
