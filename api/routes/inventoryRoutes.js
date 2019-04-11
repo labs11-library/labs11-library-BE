836b58b72721d3064ebab12b62d074b6fd5e14a0
Scott Vojik <scott.vojik@gmail.com>
2019-04-11 16:44:50 -0700

    turning off authenticate

--- api/routes/inventoryRoutes.js
+++ api/routes/inventoryRoutes.js
@@ -8,7 +8,7 @@ const Books = require("../helpers/bookModel");
 
 //GET user inventory
 
-router.get("/:userId/inventory", authenticate, async (req, res) => {
+router.get("/:userId/inventory", async (req, res) => {
   try {
     const inventory = await db("books").where({
       userId: req.params.userId
