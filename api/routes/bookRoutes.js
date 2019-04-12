99411a5fd5bf4a1237922382e6f9c0d97f829bdb
Scott Vojik <scott.vojik@gmail.com>
2019-04-12 12:34:47 -0700

    trying to fix book put bug

--- api/routes/bookRoutes.js
+++ api/routes/bookRoutes.js
@@ -84,7 +84,7 @@ router.put("/:bookId", async (req, res) => {
     const editedBook = await db("books")
       .where({ bookId: req.params.id })
       .first();
-    if (book && editedBook) {
+    if (book) {
       return res.status(200).json({ message: "Book edited!", editedBook });
     } else {
       return res.status(404).json(error);
